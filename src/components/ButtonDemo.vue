0a0b8a96ee8407730982bf0be56151c2357ec101
jianfeng <gjf610@yeah.net>
2021-12-30 15:18:24 +0800

    feat: added component Button

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Button示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button> 
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from "../lib/Button.vue";
+export default defineComponent({
+  components: {Button},
+  setup() {
+    
+  },
+})
+</script>
--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { createWebHashHistory, createRouter } from "vue-router";
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
-import Button from './components/Button.vue';
+import ButtonDemo from "./components/ButtonDemo.vue";
 import Dialog from './components/Dialog.vue';
 import Tabs from './components/Tabs.vue';
 import DocDemo from './components/DocDemo.vue';
@@ -16,7 +16,7 @@ export const router = createRouter({
       path: '/doc', component: Doc, children: [
         { path: '', component: DocDemo },
         { path: 'switch', component: SwitchDemo },
-        { path: 'button', component: Button },
+        { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: Dialog },
         { path: 'tabs', component: Tabs }
       ]
