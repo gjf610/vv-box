b6e2889b2cb3668284dab03d8ff1f23bda50f0f9
jianfeng <gjf610@yeah.net>
2021-12-30 11:50:07 +0800

    feat: Switch added toggle

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,17 @@
 <template>
-  <button><span></span></button>
+  <button :class="{checked}" @click="toggle">
+    <span></span>
+  </button>
 </template>
+<script setup lang="ts">
+import {  ref } from 'vue'
+
+const checked = ref(false)
+const toggle = () => {
+  checked.value = !checked.value
+}
+</script>
+
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
@@ -8,7 +19,7 @@ button{
   height: $h;
   width: $h*2;
   border: none;
-  background: blue;
+  background: grey;
   border-radius: calc($h/2);
   position: relative;
   > span{
@@ -20,8 +31,11 @@ button{
     background: white;
     border-radius: calc($h2/2);
   }
-  &:hover > span {
-    left: calc(100% - $h2 - 2px);
+  &.checked {
+    background: blue;
+    > span {
+      left: calc(100% - $h2 - 2px);
+    }
   }
 }
 
