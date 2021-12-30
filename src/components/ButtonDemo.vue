1eedac66af1fc76787d005fcf7014a9f72a4106e
jianfeng <gjf610@yeah.net>
2021-12-30 16:59:45 +0800

    feat: added Button level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -11,18 +11,34 @@
   <div>
     <Button size="big">big</Button>
     <Button>normal</Button>
-    <Button size="small">samll</Button>
+    <Button size="small">small</Button>
   </div>
     <div>
     <Button theme="link" size="big">big</Button>
     <Button theme="link">normal</Button>
-    <Button theme="link" size="small">samll</Button>
+    <Button theme="link" size="small">small</Button>
   </div>
     <div>
     <Button theme="text" size="big">big</Button>
     <Button theme="text">normal</Button>
-    <Button theme="text" size="small">samll</Button>
+    <Button theme="text" size="small">small</Button>
   </div>
+  <h1>示例3</h1>
+  <div>
+    <Button level="main">main button</Button>
+    <Button>normal button</Button>
+    <Button level="danger">danger button</Button>
+  </div>
+    <div>
+    <Button theme="link" level="main">main link</Button>
+    <Button theme="link">normal link</Button>
+    <Button theme="link" level="danger">danger link</Button>
+  </div>
+    <div>
+    <Button theme="text" level="main">main text</Button>
+    <Button theme="text">normal text</Button>
+    <Button theme="text" level="danger">danger text</Button>
+  </div>  
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
