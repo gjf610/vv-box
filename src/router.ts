8132b719e99a2034da921093ed7704643c0b12bd
jianfeng <gjf610@yeah.net>
2022-01-05 18:13:03 +0800

    feat: added Tabs & Tab

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import Doc from './views/Doc.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from './components/DialogDemo.vue';
-import Tabs from './components/Tabs.vue';
+import TabsDemo from './components/TabsDemo.vue';
 import DocDemo from './components/DocDemo.vue';
 
 const history = createWebHashHistory()
@@ -18,7 +18,7 @@ export const router = createRouter({
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
-        { path: 'tabs', component: Tabs }
+        { path: 'tabs', component: TabsDemo }
       ]
     }
 
