e8805d379c9303e5e6e41d6f5c2802b46130ad4b
jianfeng <gjf610@yeah.net>
2022-01-05 22:11:13 +0800

    chore: tabs selectedItem

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,7 +3,7 @@
     <div class="vv-tabs-nav" ref="container">
       <div class="vv-tabs-item" :class="{selected: t === selected}"
        v-for="(t, index) in titles" :key="index"
-       :ref="el =>{if(el) navItems[index] = el}"
+       :ref="el =>{if(t===selected) selectedItem = el}"
         @click="select(t)">{{t}}</div>
       <div class="vv-tabs-nav-indicator" ref="indicator"></div>
     </div>
@@ -23,17 +23,15 @@ export default defineComponent({
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs =navItems.value
-      const result = divs.filter(div=>div.classList.contains('selected'))[0]
-      const {width} = result.getBoundingClientRect()
+      const {width} = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width+'px'
 
       const {left: left1} = container.value.getBoundingClientRect()
-      const {left: left2} = result.getBoundingClientRect()
+      const {left: left2} = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left+'px'
     }
@@ -54,7 +52,7 @@ export default defineComponent({
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {navItems, indicator, container, defaults, titles, current, select}
+    return {selectedItem, indicator, container, defaults, titles, current, select}
   },
 })
 </script>
