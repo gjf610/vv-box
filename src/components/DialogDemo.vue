277b5487881e7c531453a990408aa8bd9249fefd
jianfeng <gjf610@yeah.net>
2022-01-05 16:15:04 +0800

    feat: added Dialog title & content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,7 +3,16 @@
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
   <Dialog v-model:visible="x" :closeOnClickOverlay="false"
-  :ok="f1" :cancel="f2"/>
+  :ok="f1" :cancel="f2">
+  <template v-slot:title>
+    <strong>加粗的标题</strong>
+  </template>
+  <template v-slot:content>
+    <strong>你好</strong>
+    <div>hi</div>    
+  </template>
+
+  </Dialog>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
     <div class="vv-dialog-wrapper">
       <div class="vv-dialog">
         <header>
-          标题
+          <slot name="title"/>
           <span class="vv-dialog-close" @click="close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content"/>
         </main>
         <footer>
           <Button level="main" @click="ok">OK</Button>
