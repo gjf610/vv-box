46cde6748a6781ea1d01992c59098de19e7da671
jianfeng <gjf610@yeah.net>
2022-01-05 18:24:46 +0800

    feat: check Tab's tag

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,21 @@
 <template>
   <div>Tabs 组件</div>
+  <component :is="defaults[0]"/>
+  <component :is="defaults[1]"/>
+
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Tab from "./Tab.vue";
+export default defineComponent({
+  setup(props, context) {
+    const defaults = context.slots.default!()
+    defaults.forEach(tag => {
+      if(tag.type !== Tab){
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
+    })
+    return {defaults}
+  },
+})
+</script>
