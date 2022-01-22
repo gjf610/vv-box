46d2c4a265e9457813309c7babbd8faeb56f5b5b
jianfeng <gjf610@yeah.net>
2022-01-22 23:13:12 +0800

    feat: ready build

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-# Vue 3 + Typescript + Vite
+# VV-Box - 一个 Vue UI 组件
 
-This template should help get you started developing with Vue 3 and Typescript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
-## Recommended IDE Setup
+## 介绍
 
-- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+这是我在学习 Vue 过程中做的一个 UI 框架，希望对你有用。
 
-## Type Support For `.vue` Imports in TS
+## 开始使用
 
-Since TypeScript cannot handle type information for `.vue` imports, they are shimmed to be a generic Vue component type by default. In most cases this is fine if you don't really care about component prop types outside of templates. However, if you wish to get actual prop types in `.vue` imports (for example to get props validation when using manual `h(...)` calls), you can enable Volar's `.vue` type support plugin by running `Volar: Switch TS Plugin on/off` from VSCode command palette.
+1. 安装 VV-box
+  ```
+  npm i --save VV-box
+  ```
+3. 引入 gulu
+  ```
+  import {Button, ButtonGroup, Icon} from 'VV-box'
+  export default {
+    name: 'app',
+    components: {
+      'vv-button': Button,
+      'vv-icon': Icon
+    }
+  }
+  ```
+
+## 文档
+
+## 提问
+
+## 变更记录
+
+## 联系方式
+
+## 贡献代码
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="vv-tabs">
     <div class="vv-tabs-nav" ref="container">
-      <div class="vv-tabs-item" :class="{selected: t === selected}"
-       v-for="(t, index) in titles" :key="index"
-       :ref="el =>{if(t===selected)selectedItem = el}"
-       @click="select(t)">{{t}}</div>
+      <div
+        class="vv-tabs-item"
+        :class="{ selected: t === selected }"
+        v-for="(t, index) in titles"
+        :key="index"
+        :ref="el => { if (t === selected) selectedItem = el }"
+        @click="select(t)"
+      >{{ t }}</div>
       <div class="vv-tabs-nav-indicator" ref="indicator"></div>
     </div>
   </div>
   <div class="vv-tabs-content">
-    <component class="vv-tabs-content-item" :is="current" :key="current.props.title"/>
+    <component class="vv-tabs-content-item" :is="current" :key="current.props.title" />
   </div>
-
 </template>
 <script lang="ts">
 import { computed, defineComponent, ref, watchEffect } from 'vue'
@@ -23,27 +26,27 @@ export default defineComponent({
     }
   },
   setup(props, context) {
-    const selectedItem = ref<HTMLDivElement>()
-    const indicator = ref<HTMLDivElement>()
-    const container = ref<HTMLDivElement>()
+    const selectedItem = ref<HTMLDivElement>();
+    const indicator = ref<HTMLDivElement>();
+    const container = ref<HTMLDivElement>();
     watchEffect(() => {
-      if(selectedItem.value&&indicator.value&&container.value){
-        const {width} = selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width = width+'px'
-        const {left: left1} = container.value.getBoundingClientRect()
-        const {left: left2} = selectedItem.value.getBoundingClientRect()
+      if (selectedItem.value && indicator.value && container.value) {
+        const { width } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const { left: left1 } = container.value.getBoundingClientRect()
+        const { left: left2 } = selectedItem.value.getBoundingClientRect()
         const left = left2 - left1
-        indicator.value.style.left = left+'px'
+        indicator.value.style.left = left + 'px'
       }
     })
     const defaults = context.slots.default!()
     defaults.forEach(tag => {
-      if(tag.type !== Tab){
+      if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
-    const current = computed(()=>{
-      return defaults.find(tag=>(tag.props!.title === props.selected))
+    const current = computed(() => {
+      return defaults.find(tag => (tag.props!.title === props.selected))
     })
     const titles = defaults.map(tag => {
       return tag.props!.title
@@ -51,7 +54,7 @@ export default defineComponent({
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {selectedItem, indicator, container, defaults, titles, current, select}
+    return { selectedItem, indicator, container, defaults, titles, current, select }
   },
 })
 </script>
@@ -79,7 +82,7 @@ $border-color: #d9d9d9;
     margin: 0 16px;
     padding: 8px 0;
     cursor: pointer;
-    &:first-child{
+    &:first-child {
       margin-left: 0;
     }
     &.selected {
